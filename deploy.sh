2bc7382efaa7758300a29dfe1049002572ef80a4
Dominic Harris <dominic@domm.me>
2021-12-15 13:54:08 -0500

    more to deploy script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,9 @@
 
 red=$(tput setaf 1)
 green=$(tput setaf 2)
+bold=$(tput bold)
 reset=$(tput sgr0)
+pwd=$(pwd)
 
 if [ "$EUID" -ne 0 ]
     then echo "This script requires root."
@@ -16,22 +18,23 @@ if [ "$EUID" -ne 0 ]
 fi
 
 if ! [ -x "$(command -v podman)" ]; then
-    echo "[${red}ERR${reset}] Podman must be installed for this script.."
+    echo "[${red}${bold}ERR${reset}] Podman must be installed for this script.."
     exit 
 else
-    echo "[${green}OK${reset}] Podman installed!"
+    echo "[${green}${bold}OK${reset}] Podman installed!"
 fi
 
 if ! [ -x "$(command -v git)" ]; then
-    echo "[${red}ERR${reset}] Git must be installed for this script.."
+    echo "[${red}${bold}ERR${reset}] Git must be installed for this script.."
     exit 
 else
-    echo "[${green}OK${reset}] Git installed!"
+    echo "[${green}${bold}OK${reset}] Git installed!"
 fi
 
 echo 
 echo "Welcome to the deploy script for Wolke.."
-echo "If you havent please review the source so you are aware of what I'm going to do!"
+echo
+echo "${red}${bold}If you havent please review the source so you are aware of what I'm going to do!${reset}"
 
 echo
 
@@ -40,16 +43,42 @@ read -p "Continue? (Y/n) " -n 1 -r
 echo  
 if [[ $REPLY =~ ^[Nn]$ ]]
 then
+        echo
         echo "Quitting!"
         exit 0
 fi
 
+if [ ! -d "api/" ]
+then   
+    echo "[${red}${bold}ERR${reset}] API directory doesn't exist, cloning it."
+    git clone --quiet https://github.com/wolke-casa/api.git
+    echo "[${green}${bold}OK${reset}] Cloned the API"
+    cd api && mv .env.example .env && cd ..
+fi
+
+if [ ! -d "bot/" ]
+then   
+    echo "[${red}${bold}ERR${reset}] Bot directory doesn't exist, cloning it."
+    git clone --quiet https://github.com/wolke-casa/bot.git
+    echo "[${green}${bold}OK${reset}] Cloned the Bot"
+    cd bot && mv .env.example .env && cd ..
+fi
+
 echo
-echo "I'll start by cloning the repos.."
+echo "[${green}${bold}OK${reset}] API and bot directories found!"
 echo
 
-git clone --quiet https://github.com/wolke-casa/api.git
-git clone --quiet https://github.com/wolke-casa/bot.git
+read -p "Did you already configure the services via the ${bold}.env${reset} file? (Y/n) " -n 1 -r
 
-echo "[${green}OK${reset}] Cloned!"
-echo 
+echo  
+if [[ $REPLY =~ ^[Nn]$ ]]
+then
+        echo
+        echo "Please configure these files in the service directories and re-run this script."
+        exit 0
+fi
+
+echo
+echo "Good!"
+echo
+
